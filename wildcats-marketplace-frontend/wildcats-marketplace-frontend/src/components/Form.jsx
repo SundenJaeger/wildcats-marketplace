325084115eb4991fbf5a407ea39d912b48740098
AmielAdriem29 <amielpenaflor123@gmail.com>
2025-09-25 20:57:40 +0800

    Updated loginSignup.jsx
    Added form validation and additional css details like error message, fonts, and separate buttons for login and signup.

--- wildcats-marketplace-frontend/wildcats-marketplace-frontend/src/components/Form.jsx
+++ wildcats-marketplace-frontend/wildcats-marketplace-frontend/src/components/Form.jsx
@@ -5,7 +5,7 @@ import LoginSignup from './LoginSignup'
 const FormLayout = () => {
   return (
     <div className="flex items-center justify-center min-h-screen p-4">
-      <div className="flex flex-col md:flex-row min-w-[500px] max-w-[1000px] bg-[#5E0E00] rounded-lg shadow-lg mb-50">
+      <div className="flex flex-col md:flex-row min-w-[500px] max-w-[1000px] bg-[#5E0E00] rounded-lg shadow-lg">
         {/* Splashscreen */}
         <div className="w-full md:w-[60%] p-6">
           <Splashscreen />
