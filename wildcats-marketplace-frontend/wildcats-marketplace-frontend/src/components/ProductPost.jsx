9e95c8836e3fb190427fecec7fab0050e24f3bae
AmielAdriem29 <amielpenaflor123@gmail.com>
2025-10-04 14:10:57 +0800

    Added reporting functionality

--- wildcats-marketplace-frontend/wildcats-marketplace-frontend/src/components/ProductPost.jsx
+++ wildcats-marketplace-frontend/wildcats-marketplace-frontend/src/components/ProductPost.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import assets from '../assets/assets'
+import ReportModal from './ReportModal'
 
 const ProductPost = ({ product, onBack }) => {
 
   const [isPostSaved, setPostSaved] = React.useState(false)
 
   const [productImageIndex, setProductImageIndex] = React.useState(0)
+
+  const [showReportModal, setShowReportModal] = React.useState(false)
 
   function decIndex(){
     setProductImageIndex( prev => prev<=0 ? 0 : prev - 1);
@@ -17,16 +20,25 @@ const ProductPost = ({ product, onBack }) => {
     console.log("increased" + productImageIndex)
   }
 
+  function handleSave(){
+    setPostSaved(!isPostSaved)
+  }
+
+  const handleReportSubmit = (ReportData) => {
+    alert('Report submitted successfully')
+    setShowReportModal(false)
+  }
+
   return (
     <div className="flex flex-col p-4 px-6 bg-[#FFF7D7]">
 
       {/* Go Back */}
-      <div className='flex justify-end items-center'>
-        <div onClick={onBack}
-            className='flex justify-center items-center w-8 h-8 bg-[#B20000] rounded-full'>
-              <input type="image" className="w-3.5 h-3.5" src={assets.white_close_icon}></input>
-            </div>
-      </div>
+        <div className='flex justify-end items-center'>
+            <div onClick={onBack  }
+                className='flex justify-center items-center w-5 h-5 bg-[#B20000] rounded-full'>
+                <input type="image" className="w-2.5  h-2.5" src={assets.white_close_icon}></input>
+                </div>
+        </div>
       
       <div className='flex justify-between gap-4 mb-2'>
         {/* Product Images */}
@@ -54,9 +66,9 @@ const ProductPost = ({ product, onBack }) => {
           <div className='flex justify-between items-center gap-2'>
             <button className='bg-[#B20000] w-full min-w-35 h-9.5 rounded-lg font-bold '>Message</button>
             <div className='flex justify-center items-center p-3 bg-[#B20000] rounded-lg'>
-              <input type="image" className="w-3.5 h-3.5" src={isPostSaved ? assets.white_save_icon : assets.white_save_icon_saved}></input>
+              <input onClick={handleSave} type="image" className="w-3.5 h-3.5" src={isPostSaved ? assets.white_save_icon : assets.white_save_icon_saved}></input>
             </div>
-            <div className='flex justify-center items-center p-3 bg-[#B20000] rounded-lg'>
+            <div onClick={() => setShowReportModal(true)} className='flex justify-center items-center p-3 bg-[#B20000] rounded-lg'>
               <input type="image" className="w-3.5 h-3.5" src={assets.white_report_icon}></input>
             </div>
           </div>
@@ -89,6 +101,14 @@ const ProductPost = ({ product, onBack }) => {
           </div>
         </div>
       </div>
+
+      {showReportModal && (
+        <ReportModal 
+          product={product}
+          onClose={() => setShowReportModal(false)}
+          onSubmit={handleReportSubmit}
+        />
+      )}
     </div>
   )
 }
---
+++ wildcats-marketplace-frontend/wildcats-marketplace-frontend/src/components/ReportModal.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import assets from '../assets/assets'
+
+const ReportModal = ({product, onClose, onSubmit}) => {
+    const [reportReason, setReportReason] = React.useState('')
+
+    const handleSubmit = () => {
+        if (!reportReason) {
+            alert("Please select a reason!");
+            return;
+        }
+        onSubmit({reason: reportReason})
+    }
+  return (
+    <div className='fixed inset-0 flex flex-col justify-center items-center bg-black/40'>
+      <div className='flex flex-col justify-between bg-[#FFF7D7] border-[#FFE26D] rounded-md p-4 w-100 h-auto'> 
+        
+        <div className='flex justify-between items-center'>
+            <h2 className='text-black font-bold text-xl'>Report: {product.name}</h2>
+            <div onClick={onClose}
+                className='flex justify-center items-center w-5 h-5 bg-[#B20000] rounded-full'>
+                <input type="image" className="w-2.5  h-2.5" src={assets.white_close_icon}></input>
+                </div>
+        </div>
+
+        {/* Line Break */}
+        <div className='flex justify-center items-center w-auto h-[1px] bg-black my-2 mx-1'></div>
+
+
+        <div className='flex flex-col items-start p-2 h-full gap-1 mb-2'>
+            <label className='text-[#5B5B5B] font-bold' value="">Select a reason...</label>
+            <button
+                type='button'
+                onClick={() => setReportReason('scam')}
+                className={`text-left pl-2 p-1 rounded-md border-gray-400 border-2 w-full bg-[#fdf9e6] hover:bg-[#e6e1cd] ${reportReason === 'scam' ? 'focus:bg-[#e6e1cd]' : ''} text-[#5B5B5B] font-bold`}
+            >Scam or Fraud</button>
+            <button
+                type='button'
+                onClick={() => setReportReason('fake')}
+                className={`text-left pl-2 p-1 rounded-md border-gray-400 border-2 w-full bg-[#fdf9e6] hover:bg-[#e6e1cd] ${reportReason === 'fake' ? 'focus:bg-[#e6e1cd]' : ''} text-[#5B5B5B] font-bold`}
+            >Counterfeit Product</button>
+            <button
+                type='button'
+                onClick={() => setReportReason('inappropriate')}
+                className={`text-left pl-2 p-1 rounded-md border-gray-400 border-2 w-full bg-[#fdf9e6] hover:bg-[#e6e1cd] ${reportReason === 'inappropriate' ? 'focus:bg-[#e6e1cd]' : ''} text-[#5B5B5B] font-bold`}
+            >Inappropriate Listing</button>
+        </div>
+
+        <div className='flex gap-1 justify-end items-center'>
+            <button className='bg-[#B20000] p-1 rounded-lg px-5 text-sm font-bold' onClick={handleSubmit}>Submit</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ReportModal
