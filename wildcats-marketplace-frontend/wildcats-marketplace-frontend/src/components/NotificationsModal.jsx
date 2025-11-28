257454b1879f6d6ff7a650f16ae3455c40d46960
AmielAdriem29 <amieljpenaflor@gmail.com>
2025-11-28 12:10:18 +0800

    Update left padding for NotificationsModal

--- wildcats-marketplace-frontend/wildcats-marketplace-frontend/src/components/NotificationsModal.jsx
+++ wildcats-marketplace-frontend/wildcats-marketplace-frontend/src/components/NotificationsModal.jsx
@@ -4,11 +4,11 @@ import assets from '../assets/assets'
 const NotificationsModal = ({onClose}) => {
   return (
     <div className='fixed inset-0 flex flex-col justify-center items-center bg-black/40 z-51'>
-      <div className='flex flex-col justify-between bg-[#fff1bd] rounded-md p-4 w-190 h-auto border-2 border-[#726948]'>
+      <div className='flex flex-col justify-between bg-[#fff1bd] rounded-md p-4 w-190 h-auto border-2 border-[#726948] gap-2 pl-5'>
 
         <div className='flex justify-between items-start'>
-            <div className='flex flex-col ml-5 mt-5 mb-2'>
-                <h2 className='text-black font-bold text-xl'>My Notifications</h2>
+            <div className='flex flex-col mt-5 mb-2'>
+                <h2 className='text-black font-bold text-2xl'>My Notifications</h2>
             </div>
                 <div onClick={onClose}
                 className='flex justify-center items-center w-5 h-5 bg-[#B20000] rounded-full'>
@@ -16,8 +16,6 @@ const NotificationsModal = ({onClose}) => {
             </div>
         </div>
 
-        {/* Line Break */}
-        <div className='flex justify-center items-center w-auto h-[1px] bg-black my-2 mx-1'></div>
 
         {/* Main Body */}
         <div className='flex justify-around items-start pb-5 h-100 gap-3'>
